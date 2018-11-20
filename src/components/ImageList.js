a3258fc6ae169e0d164c8b45ab6bfcab3a7e34a8
Remulos <liam.simpson.91@gmail.com>
2018-11-21 01:51:38 +0700

    Section Complete

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
-	const images = props.images.map(({ id, description, urls }) => {
-		return <img alt={description} src={urls.regular} key={id} />;
+	const images = props.images.map(image => {
+		return <ImageCard image={image} key={image.id} />;
 	});
 	return <div className="image-list">{images}</div>;
 };
